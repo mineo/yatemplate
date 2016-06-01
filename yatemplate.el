14d002a53d017c9d3bd4be3b12afbdfe34b2a59d
Wieland Hoffmann <themineo@gmail.com>
2016-06-01 21:26:15 +0200

    It's .dir-locals.el

--- yatemplate.el
+++ yatemplate.el
@@ -85,13 +85,13 @@ has special meaning in regular expressions."
 
 (defvar-local yatemplate-owner user-full-name
   "The copyright owner for the buffer.
-Particularly useful when combined with `dir-locals.el'.")
+Particularly useful when combined with `.dir-locals.el'.")
 
 (put 'yatemplate-owner 'safe-local-variable #'stringp)
 
 (defvar-local yatemplate-license "http://www.gnu.org/licenses/gpl.html"
   "The license (usually a URL) for the buffer.
-Particularly useful when combined with `dir-locals.el'.")
+Particularly useful when combined with `.dir-locals.el'.")
 
 (put 'yatemplate-license 'safe-local-variable #'stringp)
 
